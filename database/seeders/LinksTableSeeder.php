f838755ffe9079e884f14eb123a828f2011ce728
vagrant <vagrant@homestead>
2021-05-06 01:28:16 +0000

    侧边栏链接

---
+++ database/seeders/LinksTableSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Link;
+class LinksTableSeeder extends Seeder
+{
+    public function run()
+    {
+        Link::factory()->times(6)->create();
+    }
+}
